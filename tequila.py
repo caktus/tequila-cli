a8ad73c52aa9210d2c71d5ecc65afcf463719d35
Jeff Bradberry <jeff.bradberry@gmail.com>
2017-07-13 17:58:10 -0400

    Add 'secrets' subcommand
    Currently this only displays the decryption of the current file on disk.

--- tequila.py
+++ tequila.py
@@ -1,6 +1,7 @@
 import os.path
 import subprocess
 
+from ansible.parsing.vault import VaultLib
 import click
 
 
@@ -26,3 +27,41 @@ def play(environment, playbook):
 def install_roles():
     requirements_path = os.path.join('deployment', 'requirements.yml')
     subprocess.call(['ansible-galaxy', 'install', '-i', '-r', requirements_path])
+
+
+@cli.command(short_help="Examine the secrets for an environment.")
+@click.argument('environment')
+def secrets(environment):
+    environment_path = os.path.join('deployment', 'environments', environment)
+    secrets_path = os.path.join(environment_path, 'group_vars', 'all', 'secrets.yml')
+
+    with open('.vault_pass', 'rb') as f:
+        password = f.read().strip()
+
+    vault = VaultLib(password)
+
+    ## Read from a concrete file:
+
+    with open(secrets_path, 'rb') as f:
+        ciphertext = f.read()
+
+    ## Read from a file in the git history
+
+    # path_spec = ':'.join((revision, secrets_path))
+    # try:
+    #     ciphertext = subprocess.check_output(['git', 'show', path_spec])
+    # except Exception as e:
+    #     raise
+
+    ## Decrypt the ciphertext
+
+    try:
+        plaintext = vault.decrypt(ciphertext)
+    except Exception as e:
+        raise  # FIXME
+
+    click.echo(plaintext)
+
+    ## Alternate decryption
+
+    # subprocess.call(['ansible-vault', 'decrypt', '-'])
