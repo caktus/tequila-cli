fb53d993ba2fcd819c40526c3da55b14dc349e14
Jeff Bradberry <jeff.bradberry@gmail.com>
2017-07-13 16:35:37 -0400

    Add a 'play' subcommand
    that spawns a subprocess running the appropriate ansible-playbook command.

--- tequila.py
+++ tequila.py
@@ -1,6 +1,22 @@
+import os.path
+import subprocess
+
 import click
 
 
 @click.group()
 def cli():
     pass
+
+
+@cli.command()
+@click.argument('environment')
+@click.argument('playbook', default='site')
+def play(environment, playbook):
+    if not playbook.endswith('.yml'):
+        playbook = '{}.yml'.format(playbook)
+    playbook_path = os.path.join('deployment', 'playbooks', playbook)
+    environment_path = os.path.join('deployment', 'environments', environment)
+    inventory = os.path.join(environment_path, 'inventory')
+
+    subprocess.call(['ansible-playbook', '-i', inventory, playbook_path])
